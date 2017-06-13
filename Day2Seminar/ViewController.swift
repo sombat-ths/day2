1a3e8efb39c0f8b4344a929cff6677cd008fabce
Student05 <sombat@snru.ac.th>
2017-06-13 09:38:16 +0700

    Initial Commit

---
+++ Day2Seminar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day2Seminar
+//
+//  Created by Student05 on 6/13/2560 BE.
+//  Copyright © 2560 Sojeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
